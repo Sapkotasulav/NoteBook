d55fb3d8f8c28302ddbd04ae7b212771b04820a3
LuShIfER33 <sulavsapkota33@gmail.com>
2024-08-08 20:16:33 +0545

    feart: UI update

--- NoteBook/Form1.Designer.cs
+++ NoteBook/Form1.Designer.cs
@@ -35,22 +35,28 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnnew = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // TextField
             // 
-            this.TextField.Location = new System.Drawing.Point(96, 8);
+            this.TextField.Location = new System.Drawing.Point(13, 59);
+            this.TextField.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TextField.Multiline = true;
             this.TextField.Name = "TextField";
-            this.TextField.Size = new System.Drawing.Size(628, 354);
+            this.TextField.Size = new System.Drawing.Size(829, 386);
             this.TextField.TabIndex = 0;
             // 
             // btnsave
             // 
             this.btnsave.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnsave.Location = new System.Drawing.Point(472, 385);
+            this.btnsave.Location = new System.Drawing.Point(618, 14);
+            this.btnsave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnsave.Name = "btnsave";
-            this.btnsave.Size = new System.Drawing.Size(91, 24);
+            this.btnsave.Size = new System.Drawing.Size(121, 30);
             this.btnsave.TabIndex = 1;
             this.btnsave.Text = "Save";
             this.btnsave.UseVisualStyleBackColor = true;
@@ -59,9 +65,10 @@
             // btnopen
             // 
             this.btnopen.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnopen.Location = new System.Drawing.Point(12, 50);
+            this.btnopen.Location = new System.Drawing.Point(107, 14);
+            this.btnopen.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnopen.Name = "btnopen";
-            this.btnopen.Size = new System.Drawing.Size(67, 30);
+            this.btnopen.Size = new System.Drawing.Size(90, 37);
             this.btnopen.TabIndex = 1;
             this.btnopen.Text = "Open";
             this.btnopen.UseVisualStyleBackColor = true;
@@ -70,16 +77,18 @@
             // Filename
             // 
             this.Filename.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Italic);
-            this.Filename.Location = new System.Drawing.Point(265, 385);
+            this.Filename.Location = new System.Drawing.Point(227, 14);
+            this.Filename.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Filename.Name = "Filename";
-            this.Filename.Size = new System.Drawing.Size(125, 28);
+            this.Filename.Size = new System.Drawing.Size(165, 28);
             this.Filename.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(164, 387);
+            this.label1.Location = new System.Drawing.Point(124, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(95, 22);
             this.label1.TabIndex = 3;
@@ -87,10 +96,11 @@
             // 
             // btnnew
             // 
-            this.btnnew.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.btnnew.Location = new System.Drawing.Point(12, 12);
+            this.btnnew.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Italic);
+            this.btnnew.Location = new System.Drawing.Point(9, 12);
+            this.btnnew.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnnew.Name = "btnnew";
-            this.btnnew.Size = new System.Drawing.Size(67, 30);
+            this.btnnew.Size = new System.Drawing.Size(90, 37);
             this.btnnew.TabIndex = 1;
             this.btnnew.Text = "New";
             this.btnnew.UseVisualStyleBackColor = true;
@@ -112,26 +122,60 @@
             "jpg",
             "jpeg",
             "png"});
-            this.comboBox1.Location = new System.Drawing.Point(396, 385);
+            this.comboBox1.Location = new System.Drawing.Point(493, 13);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(70, 29);
+            this.comboBox1.Size = new System.Drawing.Size(92, 29);
             this.comboBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(397, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Extension:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.Filename);
+            this.groupBox1.Controls.Add(this.btnsave);
+            this.groupBox1.Location = new System.Drawing.Point(16, 450);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(825, 52);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(206, 10);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(631, 42);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.ClientSize = new System.Drawing.Size(746, 416);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.Filename);
+            this.ClientSize = new System.Drawing.Size(851, 506);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnnew);
             this.Controls.Add(this.btnopen);
-            this.Controls.Add(this.btnsave);
             this.Controls.Add(this.TextField);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "NoteBook";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -146,6 +190,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnnew;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
